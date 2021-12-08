e1a373721371827f2df74dd37b85b351e14162d6
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-12-08 11:29:49 -0500

    Creación de tests con mock y jest

---
+++ test/mock_jest_spyOn_restore.test.js
@@ -0,0 +1,11 @@
+import * as App from "../src/App";
+import * as math from "../src/math";
+test("calls math.add", () => {
+    const addMock = jest.spyOn(math, "add");
+    // override the implementation
+    addMock.mockImplementation(() => "mock");
+    expect(App.doAdd(1, 2)).toEqual("mock");
+    // restore the original implementation
+    addMock.mockRestore();
+    expect(App.doAdd(1, 2)).toEqual(3);
+});
